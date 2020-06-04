31ef7e5de5385733f2ea8359605ccd63e3f9322d
Chathu Vishwajith <4evercreations@gmail.com>
2020-06-04 14:36:14 +0530

    Setup Apollo provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import Layout from './containers/layout';
 import Loading from './components/loading';
 import ScrollToTop from './components/routing/scroll-top';
 import { publicRoutes } from './routes';
 import theme from './components/theme';
 
+const client = new ApolloClient({
+  uri: 'https://localhost:4000/graphql',
+});
+
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Suspense fallback={<Loading />}>
-        <Router>
-          <ScrollToTop />
-          <Switch>
-            <Layout>
-              {publicRoutes.map((route) => (
-                <Route key={route.path} {...route} />
-              ))}
-            </Layout>
-          </Switch>
-        </Router>
-      </Suspense>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Suspense fallback={<Loading />}>
+          <Router>
+            <ScrollToTop />
+            <Switch>
+              <Layout>
+                {publicRoutes.map((route) => (
+                  <Route key={route.path} {...route} />
+                ))}
+              </Layout>
+            </Switch>
+          </Router>
+        </Suspense>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 

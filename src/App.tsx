9b297c71a92f38aa2ecb0e22d4383221e4f02129
Chathu Vishwajith <4evercreations@gmail.com>
2020-06-04 13:56:30 +0530

    Add missing key routes map in app

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           <Switch>
             <Layout>
               {publicRoutes.map((route) => (
-                <Route {...route} />
+                <Route key={route.path} {...route} />
               ))}
             </Layout>
           </Switch>

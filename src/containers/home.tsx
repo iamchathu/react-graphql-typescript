53111713db311ecf7575c808df802b7e0836b96e
Chathu Vishwajith <4evercreations@gmail.com>
2020-06-04 14:45:03 +0530

    Update home to fetch data from GraphQL

--- src/containers/home.tsx
+++ src/containers/home.tsx
@@ -1,36 +1,39 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
 import Book from '../components/book';
+import Loading from '../components/loading';
 
-const books = [
-  {
-    id: '1',
-    name: 'The Davinci Code',
-    year: 2003,
-  },
-  {
-    id: '2',
-    name: 'Angels and demons',
-    year: 2003,
-  },
-  {
-    id: '3',
-    name: 'Origin',
-    year: 2003,
-  },
-];
+const ALL_BOOKS = gql`
+  query AllBooks {
+    books {
+      id
+      name
+      year
+    }
+  }
+`;
 
 const Home: React.FC = () => {
+  const { loading, error, data } = useQuery(ALL_BOOKS);
+
+  if (loading) return <Loading />;
+
+  if (error) return <>Error </>;
+
   return (
     <>
       <Typography variant='h5' component='h2'>
         Books
       </Typography>
       <Grid container spacing={2} style={{ margin: 0, width: '100%' }}>
-        {books.map(({ id, name, year }) => (
-          <Book key={id} name={name} year={year} id={id} />
-        ))}
+        {data?.books.map(
+          ({ id, name, year }: { id: string; name: string; year: number }) => (
+            <Book key={id} name={name} year={year} id={id} />
+          ),
+        )}
       </Grid>
     </>
   );
